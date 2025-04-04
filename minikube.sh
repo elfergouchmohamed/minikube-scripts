6287f938ae5aa97524929639b910e039e6ce11c2
Mohamed Elfergouch <64108561+elfergouchmohamed@users.noreply.github.com>
2025-04-04 18:41:04 +0000

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -21,7 +21,7 @@ echo "sleep 30 seconds ..."
 sleep 30
 
 # install kubelet
-echo "install kubelet ..."
+echo "install kubeclt ..."
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
